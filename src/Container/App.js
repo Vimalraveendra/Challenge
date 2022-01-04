113e2131d5c81b7b19fece44c0feda3b3f12fc86
Vimal Raveendran <vimalraveendra@gmail.com>
2022-01-04 23:52:08 +0100

    Updated App component to fetch hotel datas from third party server

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,7 +1,19 @@
-// import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App=()=> {
+  const [hotels,setHotels]=useState([])
+
+  useEffect(()=>{
+    const url=`https://obmng.dbm.guestline.net/api/hotels?collection-id=OBMNG`
+    const getHotels= async()=>{
+      const response= await fetch(url)
+      const data = await response.json();
+      setHotels(data)
+    }
+    getHotels();
+  },[])
+  console.log("hotes",hotels)
   return (
     <div className="App">
      <h1>Challenge</h1>
